b68f284bca2787a980bdf819ce42c970d82d4c53
bashirsk <user.email>
2019-05-18 13:50:52 +0100

    Initial commit

---
+++ QuranApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuranApp
+//
+//  Created by Bashir Sentongo on 18/05/2019.
+//  Copyright © 2019 Bashir Sentongo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
