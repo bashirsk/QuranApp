d56d5ea62a17ba3b871957d5bd598c00bfef458c
bashirsk <user.email>
2019-05-20 21:00:24 +0100

    Implemented Settings Screen

---
+++ QuranApp/Views/SettingsCell.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsCell.swift
+//  QuranApp
+//
+//  Created by Bashir Sentongo on 20/05/2019.
+//  Copyright © 2019 Bashir Sentongo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    @IBOutlet weak var settingsImage: UIImageView!
+    @IBOutlet weak var settingsLabel: UILabel!
+}
