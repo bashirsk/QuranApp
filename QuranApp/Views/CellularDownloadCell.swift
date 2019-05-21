cb73501adbde831df441a86da7c8be642084197f
bashirsk <user.email>
2019-05-21 21:07:21 +0100

    Created Reachability manager to check for  both Wifi and cellular connection

---
+++ QuranApp/Views/CellularDownloadCell.swift
@@ -0,0 +1,18 @@
+//
+//  CellularDownloadCell.swift
+//  QuranApp
+//
+//  Created by Bashir Sentongo on 21/05/2019.
+//  Copyright © 2019 Bashir Sentongo. All rights reserved.
+//
+
+import UIKit
+
+class CellularDownloadCell: UITableViewCell {
+    
+    @IBOutlet weak var downloadOverCellularSwitch: UISwitch!
+    
+    @IBAction func downloadOverCellularTapped(_ sender: UISwitch) {
+        //TODO
+    }
+}
